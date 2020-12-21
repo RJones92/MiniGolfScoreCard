b41cdfe9b44074ce680ebc3a61bfcab144c5a0ee
Rhys Jones <rhys.jones@paconsulting.com>
2020-12-21 14:10:25 +0000

    initial commit. Spring boot app

---
+++ src/main/java/com/example/demo/MiniGolfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGolfApplication.class, args);
+	}
+
+}
