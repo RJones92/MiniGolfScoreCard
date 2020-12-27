207139497fde0f8d8174a43dfb528fa9ab11d190
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2020-12-27 14:28:51 +0000

    Handle empty list for getScoresForPlayerId

--- src/main/java/com/golf/two_for_tom_open/controller/ScoreController.java
+++ src/main/java/com/golf/two_for_tom_open/controller/ScoreController.java
@@ -2,9 +2,12 @@ package com.golf.two_for_tom_open.controller;
 
 import com.golf.two_for_tom_open.model.Score;
 import com.golf.two_for_tom_open.service.ScoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/scores/v1")
 public class ScoreController {
-
+    private static Logger logger = LoggerFactory.getLogger(ScoreController.class);
     private final ScoreService scoreService;
 
     public ScoreController(ScoreService scoreService) {
@@ -22,7 +25,14 @@ public class ScoreController {
     @RequestMapping(value = "/player/{playerId}")
     public ResponseEntity<?> getScoresForPlayerId(@PathVariable int playerId) {
         List<Score> scores = scoreService.getScoresForPlayerId(playerId);
-        return ResponseEntity.ok(scores);
+
+        if (!scores.isEmpty()) {
+            logger.info("There are {} results returned for player ID {}.", scores.size(), playerId);
+            return ResponseEntity.ok(scores);
+        } else {
+            logger.info("There are no results for player ID {}.", playerId);
+            return ResponseEntity.noContent().build();
+        }
     }
 
 }
