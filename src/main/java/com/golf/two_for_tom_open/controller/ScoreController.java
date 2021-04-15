a7d2f27f9cd8e454b204a6309e03c9b5ca764258
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2021-04-15 20:42:17 +0100

    Remove unused endpoints from Score Controller

--- src/main/java/com/golf/two_for_tom_open/controller/ScoreController.java
+++ src/main/java/com/golf/two_for_tom_open/controller/ScoreController.java
@@ -1,14 +1,14 @@
 package com.golf.two_for_tom_open.controller;
 
 import com.golf.two_for_tom_open.model.dto.ScoreDto;
-import com.golf.two_for_tom_open.model.mapper.ScoreMapper;
 import com.golf.two_for_tom_open.service.ScoreService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Year;
 import java.util.List;
 
 @RestController
@@ -16,41 +16,15 @@ import java.util.List;
 public class ScoreController {
     private static Logger logger = LoggerFactory.getLogger(ScoreController.class);
     private final ScoreService scoreService;
-    private final ScoreMapper scoreMapper;
 
-    public ScoreController(ScoreService scoreService, ScoreMapper scoreMapper) {
+    public ScoreController(ScoreService scoreService) {
         this.scoreService = scoreService;
-        this.scoreMapper = scoreMapper;
     }
 
     @GetMapping(value = {"/", ""})
     public ResponseEntity<?> getScores() {
         List<ScoreDto> scores = scoreService.getAllScoreDto();
-        return getResponseEntityFromListOfScores(scores, " all scores");
-    }
-
-    @GetMapping(value = "/player/{playerId}")
-    public ResponseEntity<?> getScoresForPlayerId(@PathVariable int playerId) {
-
-        List<ScoreDto> scores = scoreService.getScoresForPlayerById(playerId);
-
-        return getResponseEntityFromListOfScores(scores, "player ID " + playerId);
-    }
-
-    @GetMapping(value = "/player")
-    public ResponseEntity<?> getScoresForPlayerName(@RequestParam String firstName, @RequestParam String lastName) {
-
-        List<ScoreDto> scores = scoreService.getScoresForPlayerByName(firstName, lastName);
-
-        return getResponseEntityFromListOfScores(scores, "player " + firstName + " " + lastName);
-    }
-
-    @GetMapping(value = "/tournaments/{tournamentYear}")
-    public ResponseEntity<?> getScoresForTournamentYear(@PathVariable int tournamentYear) {
-        List<ScoreDto> scores = scoreService.getScoresForTournamentByYear(Year.of(tournamentYear));
-
-        return getResponseEntityFromListOfScores(scores, "tournament year " + tournamentYear);
-
+        return getResponseEntityFromListOfScores(scores, "all scores");
     }
 
     private ResponseEntity<?> getResponseEntityFromListOfScores(List<ScoreDto> scores,String textToIdentifyRequestedObjects) {
--- src/main/ui/src/services/scoreService.js
+++ src/main/ui/src/services/scoreService.js
@@ -4,12 +4,4 @@ async function getAllScores() {
   return await response.json();
 }
 
-async function getScoresByTournamentYear(tournamentYear) {
-  const response = await fetch("/api/scores/tournament/" + tournamentYear);
-  console.log(
-    "Request sent to retrieve scores for tournament year " + tournamentYear
-  );
-  return await response.json();
-}
-
-export { getAllScores, getScoresByTournamentYear };
+export { getAllScores };
