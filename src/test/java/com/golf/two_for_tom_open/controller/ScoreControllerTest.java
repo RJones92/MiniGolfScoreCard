93f2dc20f12b1ca3af9610d7b6a4f69493bfcfc4
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2021-04-15 20:46:52 +0100

    Add score controller unit tests. #15

---
+++ src/test/java/com/golf/two_for_tom_open/controller/ScoreControllerTest.java
@@ -0,0 +1,48 @@
+package com.golf.two_for_tom_open.controller;
+
+import com.golf.two_for_tom_open.model.dto.ScoreDto;
+import com.golf.two_for_tom_open.service.ScoreService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(MockitoExtension.class)
+class ScoreControllerTest {
+
+    @InjectMocks
+    ScoreController scoreController;
+    @Mock
+    ScoreService scoreService;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(scoreController).build();
+    }
+
+    @Test
+    void getScores() throws Exception {
+        when(scoreService.getAllScoreDto()).thenReturn(Arrays.asList(new ScoreDto()));
+
+        mockMvc.perform(get("/api/scores"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+        verify(scoreService, times(1)).getAllScoreDto();
+    }
+}
