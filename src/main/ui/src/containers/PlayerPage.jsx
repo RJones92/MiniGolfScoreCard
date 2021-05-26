e111ac91ad0bfe2a1758af482c8335169615be88
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2021-05-26 21:33:47 +0100

    Skeleton code added to frontend. #39

--- src/main/ui/src/containers/PlayerPage.jsx
+++ src/main/ui/src/containers/PlayerPage.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllPlayers } from "../services/playerService";
 
 function PlayerPage(props) {
-  return <p>Placeholder - not yet implemented.</p>;
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getAllPlayers().then(
+      (allPlayers) => {
+        setIsLoaded(true);
+        //DO STUFF
+      },
+      (errorMessage) => {
+        setIsLoaded(true);
+        setError(errorMessage);
+      }
+    );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return <p>Placeholder - not yet implemented.</p>;
+  }
 }
 
 export default PlayerPage;
