0810830eb882f1e77e9b0d967954beed9fbe3f41
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2021-06-20 21:43:53 +0100

    Add all player stats into an object. #39

--- src/main/ui/src/containers/PlayerPage.jsx
+++ src/main/ui/src/containers/PlayerPage.jsx
@@ -1,22 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { getAllPlayers } from "../services/playerService";
 
+const statTypes = new Map([
+  ["countOfTournamentsPlayed", "Tournaments played"],
+  ["countOfTournamentsWon", "Tournaments won"],
+  ["countOfCoursesPlayed", "Courses played"],
+  ["countOfCoursesWon", "Courses won"],
+  ["countOfHolesPlayed", "Holes played"],
+  ["countOfHolesWon", "Holes won"],
+]);
+
 function PlayerPage(props) {
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
+  const [playerStatsTableObjects, setPlayerStatsTableObjects] = useState([]);
 
   useEffect(() => {
     getAllPlayers().then(
       (allPlayers) => {
         setIsLoaded(true);
-        //DO STUFF
+        setPlayerStatsTableObjects(createStatsTable(allPlayers));
       },
       (errorMessage) => {
         setIsLoaded(true);
         setError(errorMessage);
       }
     );
-  }, []);
+
+    function createStatsTable(allPlayers) {
+      const playerKeys = Object.keys(allPlayers);
+      let playerColumns = playerKeys.forEach((playerKey) => {
+        const player = allPlayers[playerKey];
+        return createPlayerColumn(player);
+      });
+
+      return {
+        headerColumn: ["", ...statTypes.values()],
+        playerColumns: playerColumns,
+      };
+    }
+
+    function createPlayerColumn(player) {
+      const column = [];
+      column.push(player.firstName + " " + player.lastName);
+
+      for (const statName of statTypes.values()) {
+        column.push(statName);
+      }
+
+      return column;
+    }
+  }, [playerStatsTableObjects]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
